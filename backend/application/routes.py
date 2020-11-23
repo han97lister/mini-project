ee3ff243b48cadc85fe87fee04076cbdc864cfb4
Hannah Lister-Sims <hlistersims@gmail.com>
2020-11-23 17:00:34 +0000

    started frontend app

---
+++ backend/application/routes.py
@@ -0,0 +1,23 @@
+from application import app
+from flask import Flask, Response, request, jsonify
+import requests
+import random
+
+
+animals = {'Cow':'Moooo', 'Dog':'Woof woof!', 'Sheep':'Baabaa', 'Cat':'meow', 'Lion':'RAWR'}
+
+@app.route('/animal', methods=['GET'])
+def get_animal():
+    
+    animal = random.choice(animals).key()
+    return jsonify({"animal":animal})
+
+@app.route('/animal/<string:animal>', methods=['POST'])
+def post_noise(animal):
+    
+    animal = request.get_json()['animal'].lower()
+    noise = animals.get(animal)
+    return jsonify({"noise": noise})
+
+
+
