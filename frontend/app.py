3e7f007b75b9eb72a3c6694b6ed940deafe04008
Hannah L-S <hlistersims@gmail.com>
2020-11-24 15:05:38 +0000

    uodate

--- frontend/app.py
+++ frontend/app.py
@@ -2,16 +2,16 @@ from flask import Flask, render_template
 import requests
 
 app = Flask(__name__)
-api = "http://backend:5001"
+#api = "http://backend:5001"
 
 @app.route('/', methods=['GET'])
 def index():
     
-    animal_request = requests.get("htpp://localhost:5001/animal")
-    animal = request.json()['animal']
+    animal_request = requests.get("http://localhost:5001/animal")
+#    animal = request.json()['animal']
 
-    noise_request = requests.post("http://localhost:5001/noise", json={"animal": animal})
-    noise = noise_request.json()['noise']
+    noise_request = requests.post("http://localhost:5001/noise", data=animal.text)
+ #   noise = noise_request.json()['noise']
     return render_template('index.html', animal=animal.text, noise=noise.text)
 
 if __name__ == '__main__':
