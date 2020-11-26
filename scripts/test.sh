6f3e3595f21021b626ff4c5db55bf9fd1e0dfa9a
Hannah Lister-Sims <hlistersims@gmail.com>
2020-11-26 12:29:05 +0000

    complete scripts for jenkins build

---
+++ scripts/test.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+sudo apt install python3 python3-pip python3-venv -y
+
+python3 -m venv venv
+. venv/bin/activate
+
+pip3 install -r requirements.txt
+
+pytest --cov ./frontend --cov-report term-missing
+pytest --cov ./backend/application --cov-report term-missing
+
+deactivate
+rm -rf venv
